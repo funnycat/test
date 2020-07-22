b8eba157fd0d578d2d56f56a027c698755609d53
Emily Yuhas <emilymyang@gmail.com>
2020-07-22 14:12:02 -0700

    3rd  commit

--- email-widget.js
+++ email-widget.js
@@ -1,4 +1,6 @@
 jQuery(document).ready(function() {	
-		$('.product-form__controls-group--submit').prepend('<button type="button" class="shopify-payment-button shopify-payment-button__button--unbranded" data-testid="Checkout-button">Preorder Now</button>')
+		$('.product-form__item').prepend('<button type="submit" class="shopify-payment-button" style="display: block; width: 100%; line-height: 1.4; padding-left: 5px; padding-right: 5px; white-space: normal; margin-top: 0; margin-bottom: 10px; min-height: 44px;" data-testid="Checkout-button">Preorder Now</button>')
 		console.log('Button Script Tag Added!');
 });
+
+
