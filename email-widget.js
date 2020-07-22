9ff3e05b1e30a12557d1c0720d5d00c4b62dedc2
Emily Yuhas <emilymyang@gmail.com>
2020-07-22 13:32:46 -0700

    2nd commit

--- email-widget.js
+++ email-widget.js
@@ -1,18 +1,4 @@
 jQuery(document).ready(function() {	
-		$('body').prepend('<div class="header" id="myHeader"><h1> test </h1></div>');
-		$('head').prepend('<style>.header { padding: 10px 16px; background: #555; color: #f1f1f1; } .content { padding: 16px; } .sticky { position: fixed; top: 0; width: 100%} .sticky + .content { padding-top: 102px; }</style>');
-		
-		console.log('Email Script Tag Added!');
-
-		var header = document.getElementById("myHeader");
-		var sticky = header.offsetTop;
-
-		window.onscroll = function() { 
-			if (window.pageYOffset > sticky) {
-				header.classList.add("sticky");
-			} else {
-				header.classList.remove("sticky");
-			}
-		};
-  
+		$('.product-form__controls-group--submit').prepend('<button type="button" class="shopify-payment-button" data-testid="Checkout-button">Preorder Now</button>')
+		console.log('Button Script Tag Added!');
 });
