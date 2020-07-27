b3c83653f53545debc868cb3d5826f42749ab9e2
Emily Yuhas <emilymyang@gmail.com>
2020-07-27 14:00:37 -0700

    more logging

--- email-widget.js
+++ email-widget.js
@@ -9,10 +9,11 @@ jQuery(document).ready(function() {
 
 		$.get(requestPath)
 		  .done(function( data ) {
-		  	
+
 			var stringified = JSON.stringify(data);
 		    var preorderDetails = JSON.parse(stringified);
-		    alert( "Data Loaded: " + preorderDetails );
+		    console.log( "Data Loaded: " + stringified );
+		    console.log( "Data Loaded: " + preorderDetails );
 
 		    $('.product-form__item--payment-button').prepend('<div class="product-form__item product-form__item--submit product-form__item--payment-button"><button type="submit" class="shopify-payment-button" style="display: block; width: 100%; font-size: 16px; font-family: font-family: "Helvetica Neue", Helvetica, Arial, sans-serif; line-height: 1.4; padding: 8px; margin-top: 0; margin-bottom: 10px; min-height: 44px; background-color: #000; color: #3a3a3a; border-color: #3a3a3a;" data-testid="Checkout-button">preorderDetails</button></div>')
 			
