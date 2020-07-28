7aaa5b2b8aaa38dadea1447981b266e22737468d
Emily Yuhas <emilymyang@gmail.com>
2020-07-27 20:22:18 -0700

    removing stuff

--- email-widget.js
+++ email-widget.js
@@ -12,17 +12,20 @@ jQuery(document).ready(function() {
 
 			var stringified = JSON.stringify(data);
 		    var preorderDetails = JSON.parse(stringified);
-		    console.log( "Data Loaded1: " + stringified );
-		    console.log( "Data Loaded2: " + preorderDetails );
-		    console.log( "button text1: " + preorderDetails.buttonText );
+		    console.log( "Data Loaded: " + stringified );
 
 		    const parentDivClasses = 'product-form__item product-form__item--submit product-form__item--payment-button';
 		    const buttonStyle = 'display: block; width: 100%; font-size: 16px; font-family: font-family: "Helvetica Neue", Helvetica, Arial, sans-serif; line-height: 1.4; padding: 8px; margin-top: 10px; margin-bottom: 10px; min-height: 44px; background-color: #000; color: #3a3a3a; border-color: #3a3a3a;';
 		    const buttonClass = 'shopify-payment-button';
 		    const message = '<p>'+preorderDetails.messageText+'</p>'
 
-		    $('.product-form__item--payment-button').prepend('<div class="'+parentDivClasses+'"><button type="submit" class="" style="'+buttonStyle+'" data-testid="Checkout-button">'+preorderDetails.buttonText+'</button>'+message+'</div>')
+//		    $('.product-form__item--payment-button').prepend('<div class="'+parentDivClasses+'"><button type="submit" class="'+buttonClass+'" style="'+buttonStyle+'" data-testid="Checkout-button">'+preorderDetails.buttonText+'</button>'+message+'</div>')
 			
+			$('.product-form__controls-group--submit').prepend('<div class="'+parentDivClasses+'"><button type="submit" class="'+buttonClass+'" style="'+buttonStyle+'" data-testid="Checkout-button">'+preorderDetails.buttonText+'</button>'+message+'</div>')
+			
+
+			$(product-form__item--payment-button).empty();
+
 			console.log('Button Added!');
 
 		  }).fail(function(){ 
