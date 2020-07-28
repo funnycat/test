ab182f97e3f581d1bb73ef8606b4cb7e663e3bcb
Emily Yuhas <emilymyang@gmail.com>
2020-07-27 21:23:50 -0700

    disalbed button

--- email-widget.js
+++ email-widget.js
@@ -22,8 +22,14 @@ jQuery(document).ready(function() {
 //		    $('.product-form__item--payment-button').prepend('<div class="'+parentDivClasses+'"><button type="submit" class="'+buttonClass+'" style="'+buttonStyle+'" data-testid="Checkout-button">'+preorderDetails.buttonText+'</button>'+message+'</div>')
 			
 			$('.product-form__item--payment-button').hide();
-			$('.product-form__controls-group--submit').prepend('<div class="'+parentDivClasses+'"><button type="submit" class="'+buttonClass+'" style="'+buttonStyle+'" data-testid="Checkout-button">'+preorderDetails.buttonText+'</button>'+message+'</div>')
-			
+
+			if(preorderDetails.goalReached){
+				$('.product-form__controls-group--submit').prepend('<div class="'+parentDivClasses+'"><button type="submit" class="'+buttonClass+'" style="'+buttonStyle+'" data-testid="Checkout-button" disabled>'+preorderDetails.disabledButtonText+'</button>'+message+'</div>')	
+	
+			}else{
+				$('.product-form__controls-group--submit').prepend('<div class="'+parentDivClasses+'"><button type="submit" class="'+buttonClass+'" style="'+buttonStyle+'" data-testid="Checkout-button">'+preorderDetails.buttonText+'</button>'+message+'</div>')	
+			}
+		
 
 			console.log('Button Added!');
 
